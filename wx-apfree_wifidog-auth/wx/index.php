2d0428b621d9eade2771b868713cbabbe6d63f1e
wwz09 <wwz5124@gmail.com>
2020-07-01 08:12:43 +0800

    wwz09

---
+++ wx-apfree_wifidog-auth/wx/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head lang="zh-CN">
+    <meta charset="UTF-8">
+    <title>黄花旅游</title>
+</head>
+<body>
+	<script type="text/javascript">
+	window.location="http://192.168.1.1:2060/wifidog/auth?token=123"
+	</script>
+</body>
+</html>
