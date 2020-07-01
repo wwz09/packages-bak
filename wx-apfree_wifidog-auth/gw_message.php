57b9811eb0f60531593d06c24f650ee33bdacc6b
wwz09 <wwz5124@gmail.com>
2020-07-01 08:33:56 +0800

    wwz09

--- wx-apfree_wifidog-auth/gw_message.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $message = null;
-    if(isset($_GET["message"])){
-        $message = $_GET["message"];
-    }
-    echo $message;
-?>
--- wx-apfree_wifidog-auth/portal/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-//认证前用户访问任意url，然后被重定向登录页面，session记录的是这个“任意url”.
-$url = "http://pmgeco.com";
-//跳转到登陆前页面.
- header("Location: ".$url);
-?>
